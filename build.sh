8b245c60c9f9538cab7c52c95da52453799dbda0
mohammedaouamri5 <mohammedaouamri5@gmail.com>
2024-09-01 12:28:06 +0100

    add ai

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+BINARY="tmp/main"
+LOG="tmp/build.log"
+
+# Define the build target
+function build() {
+	touch $db	
+	go build -o $BINARY . 2>&1 | tee $LOG
+}
+# Define the clean target
+function clean() {
+	rm -rfv tmp 
+}
+
+clean; 
+build; 
